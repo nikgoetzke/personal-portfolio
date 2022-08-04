971b60585f6eda2ac7d87bf4e7211f8f502cb4d0
Nik Goetzke <nikgoetzke@gmail.com>
2022-08-04 01:32:18 -0500

    Import banner to App.js and created component for banner Banner.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,13 @@
 // import bootstrap from "bootstrap";
 import { useState, useEffect } from "react";
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
-
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const NavBar = () => {
-  const [activeLink, setActiveLink] = useState('home');
+  const [activeLink, setActiveLink] = useState("home");
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
@@ -14,38 +17,71 @@ export const NavBar = () => {
       } else {
         setScrolled(false);
       }
-    }
-  
+    };
 
-  window.addEventListener("scroll", onScroll);
+    window.addEventListener("scroll", onScroll);
 
-  return () => window.removeEventListener("scroll", onScroll);
-  }, [])
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
-const onUpdateActiveLink = (value) => {
-  setActiveLink(value);
-}
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
 
   return (
     <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
-        <Navbar.Brand href="#home">Nik Goetzke</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img src={logo} alt="logo" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home" className={activeLink === "home" ? "active navbar-link" : "navbar-link"}onClick={()=> onUpdateActiveLink("home")}>Home</Nav.Link>
-            <Nav.Link href="#skills" className={activeLink === "skills" ? "active navbar-link" : "navbar-link"} onClick={()=> onUpdateActiveLink("skills")}>Skills</Nav.Link>
-            <Nav.Link href="#projects" className={activeLink === "projects" ? "active navbar-link" : "navbar-link"} onClick={()=> onUpdateActiveLink("projects")}>Projects</Nav.Link>
+            <Nav.Link
+              href="#home"
+              className={
+                activeLink === "home" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("home")}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#skills"
+              className={
+                activeLink === "skills" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("skills")}
+            >
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              href="#projects"
+              className={
+                activeLink === "projects" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("projects")}
+            >
+              Projects
+            </Nav.Link>
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-              <a href="#"><img src={} alt=""/></a>
-              <a href="#"><img src={} alt=""/></a>
-              <a href="#"><img src={} alt=""/></a>
+              <a href="#">
+                <img src={navIcon1} alt="" />
+              </a>
+              <a href="#">
+                <img src={navIcon2} alt="" />
+              </a>
+              <a href="#">
+                <img src={navIcon3} alt="" />
+              </a>
             </div>
-            <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
+            <button className="vvd" onClick={() => console.log("connect")}>
+              <span>Let's Connect</span>
+            </button>
           </span>
         </Navbar.Collapse>
       </Container>
